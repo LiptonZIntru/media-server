06cd98a0a15bbfe709098b1ff10b86161f977f57
vili nguyen <vili2002@centrum.cz>
2020-07-15 13:04:08 +0200

    csfd description and rating added

--- app/controllers/film.py
+++ app/controllers/film.py
@@ -7,6 +7,8 @@ import moviepy
 from moviepy.editor import VideoFileClip
 import datetime
 import os
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
@@ -79,9 +81,21 @@ def show(request, id):
     film = Film.objects.filter(id=id)[0]
     film.duration = datetime.timedelta(seconds=int(film.duration))
     film.film_url = "uploaded_films/" + film.film_url
+
+    try:
+        csfd_page = requests.get(film.csfd_link, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'})
+        soup = BeautifulSoup(csfd_page.text, 'html.parser')
+        csfd_description = soup.body.find(id="plots").select('div > div')[1].ul.li.select('li > div')[0].get_text()
+        csfd_rating = soup.body.find(id="rating").h2.get_text()
+    except:
+        csfd_description = None
+        csfd_rating = None
+
     return render(request, 'films/show.html',
                   {
                       'film': film,
+                      'csfd_description': csfd_description,
+                      'csfd_rating': csfd_rating,
                   })
 
 
